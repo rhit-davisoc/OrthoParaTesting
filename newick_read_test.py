354d28a00d26d05e41dea7fc36caadcce813cf0c
rhit-davisoc <78520741+rhit-davisoc@users.noreply.github.com>
2023-06-15 14:30:58 -0400

    Added ability method to print matrix of relationships. Added comments for clarity and renamed methods / reformatted code. Commented out print statements to visualize tree.

--- newick_read_test.py
+++ newick_read_test.py
@@ -8,10 +8,8 @@ tree = dendropy.Tree.get(
     data=s1,
     schema="newick")
 
-rel_dict = {}
-
-print(tree.as_string(schema="newick",) + "\n")
-print(tree.as_ascii_plot())
+# print(tree.as_string(schema="newick",) + "\n")
+# print(tree.as_ascii_plot())
 
 def get_geneid(label):
     if "-" in label:
@@ -26,10 +24,11 @@ def get_species(label):
         return ""
 
 # Label pairs of taxons paralogs or orthologs
-# Orthologs -> Have a least common ancestor, is a result of speciation
-# Paralogs -> Result of duplication (LCA is a duplication event)
+# Orthologs -> Result of a speciation event
+# Paralogs -> Result of a duplication event
 
 def label_tree_events(tree, start=1.0):
+    rel_dict = {}
     for node in tree.postorder_node_iter():
         if node.is_leaf():
             node.species = [get_species(str(node.taxon))]
@@ -37,23 +36,27 @@ def label_tree_events(tree, start=1.0):
             node.clade = [str(node.taxon)]
             rel_dict[str(node.taxon)] = {} 
         else:
-            assign_event(node)
-    return tree
+            assign_relationships(node, rel_dict)
+    return rel_dict
 
-
-def assign_event(node):
+# Assign paralogous or orthologous relationship(s) at a given node
+def assign_relationships(node, rel_dict):
     if (node.num_child_nodes() == 2):
         children = node.child_nodes()
         event = ""
 
-        #Speciation occured, label orthologs
+        #Speciation occured, label combination of species in each clade orthologous
+        #(Ex. Child 1 has species {man-a, man-b} and child 2 has species {mouse-a,mouse-b}, 
+        #so the combinations {man-a,mouse-a}, {man-a,mouse-b}, {man-b,mouse-a}, {man-b,mouse-b} would be orthologs)
         if not set(children[0].species) & set(children[1].species):
-            event = "orthologs"
+            event = "orthologous"
         else:
-            event = "paralogs"
+            #Duplication occured, label combinations of species at this node paralogous
+            event = "paralogous"
 
         for tax_1,tax_2 in itertools.product(children[0].clade,children[1].clade):
-            print("%s and %s are %s" % (tax_1,tax_2,event))
+            #Print out line by line as orthologous/paralogous relationships are found
+            #print("%s and %s are %s" % (tax_1,tax_2,event))
             rel_dict[tax_1][tax_2] = event
             rel_dict[tax_2][tax_1] = event
         
@@ -61,12 +64,26 @@ def assign_event(node):
         node.clade = children[0].clade + children[1].clade
 
 
-def print_2d_dict(dic,children):
+# Given a 2d dictionary of relationships, print out a the information in a matrix
+def print_2d_dict(rel_dict):
+    child_list = rel_dict.keys()
+
+    print(f"{'':<10}", end = " ")
+    for child in child_list:
+        print("\t" + child, end = " ")
+
+    print("\n")
     
-    return
+    for child_row in child_list:
+        print(f"{child_row:<15}", end = " ")
+        for child_col in child_list:
+            if(child_row != child_col):
+                print(rel_dict[child_row][child_col] + "\t", end = " ")
+            else:
+                print("\t\t",end = " ")
+        print("\n")
+        
 
+rel_dict = label_tree_events(tree)
 
-label_tree_events(tree)
-
-# for node in tree.postorder_node_iter():
-#     print("%s : %s" % (node.taxon, get_geneid(str(node.taxon))))
+print_2d_dict(rel_dict)
