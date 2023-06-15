ae2d4537b0b9fa04c28da43f315ea841e9c18c0f
rhit-davisoc <78520741+rhit-davisoc@users.noreply.github.com>
2023-06-15 13:45:35 -0400

    Label orthologous/paralogous relationships of binary trees with no ambigious cases (i.e. can't determine speciation or duplication event without more information)

--- newick_read_test.py
+++ newick_read_test.py
@@ -1,14 +1,17 @@
 import dendropy
+import itertools
 
-# Simple test tree in newick format (Begins with duplication of a and b gene, then two subsequent speciation events (man and mouse))
-s1 = "((man-a,mouse-a),(man-b,mouse-b));"
+# Tree to test with
+s1 = "((man-a,man-b),((mouse-a,zebra-a),(mouse-c,deer-c)));"
+
 tree = dendropy.Tree.get(
     data=s1,
     schema="newick")
 
+rel_dict = {}
+
 print(tree.as_string(schema="newick",) + "\n")
 print(tree.as_ascii_plot())
-
 
 def get_geneid(label):
     if "-" in label:
@@ -16,23 +19,23 @@ def get_geneid(label):
     else:
         return ""
 
-
 def get_species(label):
     if "-" in label:
         return label.split('-')[0]
     else:
         return ""
 
-# Label the events at each node (not including leaf nodes) to be either speciation or duplication
-# Speciation -> Leads to different taxonomy (e.g. results in man and mouse split)
-# Duplication -> Gene duplication (e.g. hemoglobin a and homoglobin b split)
+# Label pairs of taxons paralogs or orthologs
+# Orthologs -> Have a least common ancestor, is a result of speciation
+# Paralogs -> Result of duplication (LCA is a duplication event)
 
 def label_tree_events(tree, start=1.0):
     for node in tree.postorder_node_iter():
         if node.is_leaf():
-            node.species = get_species(str(node.taxon))
-            node.gene = get_geneid(str(node.taxon))
-            node.event = "leaf"
+            node.species = [get_species(str(node.taxon))]
+            node.gene = [get_geneid(str(node.taxon))]
+            node.clade = [str(node.taxon)]
+            rel_dict[str(node.taxon)] = {} 
         else:
             assign_event(node)
     return tree
@@ -41,19 +44,29 @@ def label_tree_events(tree, start=1.0):
 def assign_event(node):
     if (node.num_child_nodes() == 2):
         children = node.child_nodes()
-        if (children[0].species == children[1].species):
-            node.event = "duplication"
-            node.species = ""
-            node.gene = ""
+        event = ""
+
+        #Speciation occured, label orthologs
+        if not set(children[0].species) & set(children[1].species):
+            event = "orthologs"
         else:
-            node.event = "speciation"
-            node.species = ""
-            node.gene = ""
-    else:
-           node.event = ""
+            event = "paralogs"
+
+        for tax_1,tax_2 in itertools.product(children[0].clade,children[1].clade):
+            print("%s and %s are %s" % (tax_1,tax_2,event))
+            rel_dict[tax_1][tax_2] = event
+            rel_dict[tax_2][tax_1] = event
+        
+        node.species = children[0].species + children[1].species
+        node.clade = children[0].clade + children[1].clade
+
+
+def print_2d_dict(dic,children):
+    
+    return
 
 
 label_tree_events(tree)
 
-for node in tree.postorder_node_iter():
-    print("%s : %s : %s" % (node.taxon, node.event, get_geneid(str(node.taxon))))
+# for node in tree.postorder_node_iter():
+#     print("%s : %s" % (node.taxon, get_geneid(str(node.taxon))))
